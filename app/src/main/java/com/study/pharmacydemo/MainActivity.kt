29cbd5971b2cb31641764cc1703bc48dc62acc02
btWin9527 <41253028+btWin9527@users.noreply.github.com>
2022-02-21 09:27:42 +0800

    新增okhttp

--- app/src/main/java/com/study/pharmacydemo/MainActivity.kt
+++ app/src/main/java/com/study/pharmacydemo/MainActivity.kt
@@ -2,10 +2,37 @@ package com.study.pharmacydemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import okhttp3.*
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        getPharmaciesData()
+    }
+    private fun getPharmaciesData(){
+        val pharmaciesDataUrl=""
+        val okhttpClient = OkHttpClient().newBuilder().build()
+        val request = Request.Builder().url(pharmaciesDataUrl).get().build()
+        val call=okhttpClient.newCall(request)
+        call.enqueue(object:Callback{
+            override fun onFailure(call: Call, e: IOException) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                Log.d("MainActivity","onResponse${response.body.toString()}")
+            }
+
+        }
+
+        )
+
+
+
+
+
     }
 }
